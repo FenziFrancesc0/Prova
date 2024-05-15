06b2bec731d4172ed1959dad8315cc57a154270c
Fabio Calzavara <fabio.calzavara@itsrizzoli.it>
2024-05-15 12:23:47 +0200

    fabio

--- lista.py
+++ lista.py
@@ -1,2 +1,13 @@
-if x % 7 == 0:
-    print("buzz")
+minimo = input(int("inserisci il minimo"))
+massimo = input(int("inserisci il massimo"))
+somma = 0
+lista = []
+while minimo < massimo:
+    if minimo % 7 == 0:
+        lista.append("buzz")
+    else: 
+        lista.append(minimo)
+        somma = somma + minimo
+    minimo += 1
+print(somma)
+print(lista)
